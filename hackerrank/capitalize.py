7776e3e1bb5238545e43aa0d63d609a23d669edc
N-O-B-C <90341262+N-O-B-C@users.noreply.github.com>
2022-06-20 09:28:39 +0100

    complte work on capitalizing first alphabet of words

--- hackerrank/capitalize.py
+++ hackerrank/capitalize.py
@@ -1,5 +1,36 @@
-def solve():
-    name = input("Enter name: ")
-    name = name.capitalize()
-    print(name)
-solve()
+# string = "python pool"
+# print("original string:")
+# print(string)
+# print("After capitalization first leeter:")
+# print(string.capitalize())
+
+
+# string = "python pool"
+# print("original string:")
+# print(string)
+# result = string[0].upper() + string[1:]
+# print("After capitalization first letter:")
+# print(result)
+
+
+# string = "python pool"
+# print("Original string:")
+# print(string)
+# print("After capitalizing:")
+# print(str.title(string))
+
+# string = "python pool"
+# print("Original string:")
+# print(string)
+# print("After capitalizing first letter:")
+# result = ' '.join(elem.capitalize() for elem in string.split())
+# print(result)
+
+
+import string
+txt = "python pool"
+print("Original string:")
+print(txt)
+print("After capitalizing first letter:")
+result = string.capwords(txt)
+print(result)
