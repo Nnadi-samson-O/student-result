c4df4b9acd3af41c5920721576d1bc371a21ad8f
N-O-B-C <90341262+N-O-B-C@users.noreply.github.com>
2022-06-23 12:39:47 +0100

    looked at the previous class student grade

---
+++ practicing/compare.py
@@ -0,0 +1,20 @@
+# x = int(input("whats x: "))
+# y = int(input("what's y: "))
+
+# if x < y:
+#     print("x is less than y")
+
+# elif x > y:
+#     print("x is greater than y")
+
+# else:   
+#     print("x is equal to y")
+
+
+x = int(input("whats x: "))
+y = int(input("what's y: "))
+
+if x != y:
+    print("x is not equal to y")
+else:
+    print("x is egual to y")
