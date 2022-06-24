bacd78f75272b9fd412874056ef52da7212f58fc
N-O-B-C <90341262+N-O-B-C@users.noreply.github.com>
2022-06-24 20:23:46 +0100

    worked on agree statement

---
+++ practicing/work.py
@@ -0,0 +1,9 @@
+#The function below accept a value weather up or lower.
+s = input("do you agree? \n").lower()
+
+if s in ['y', 'yes']:
+    print("Areed")
+elif s() in ['n', 'no']:
+    print("Not agreed")
+
+ 
