0d2e3eadf5af2b021f13870735b5089a503b2ec6
N-O-B-C <90341262+N-O-B-C@users.noreply.github.com>
2022-06-28 02:35:24 +0100

    calendar modules in different form

--- hackerrank/calendar_module.py
+++ hackerrank/calendar_module.py
@@ -1,5 +1,9 @@
 import calendar
 
+# dec = calendar.TextCalendar(calendar.SUNDAY)
+# dec.prmonth(2020, 12)
+
+
 month_day_year = input("Enter date press shift, Enter month press shift and Enter year. Then press enter. \n")
 month_day_year = month_day_year.split()
 
@@ -15,7 +19,7 @@ if cal == 0:
 
 elif cal == 1:
     print("TUESDAY")  
-      
+
 elif cal == 2:
     print("WEDNESDAY") 
 
@@ -30,3 +34,36 @@ elif cal == 5:
 
 elif cal == 6:
     print("SUNDAY")
+
+
+# print("============================================")
+# print(">>>>>>>>>>>> LEAP YEAR CALCULATOR <<<<<<<<<<<")
+# year1 = int(input("Enter the first year: "))
+# year2 = int(input("Enter the second year: "))
+
+# leaps = calendar.leapdays(year1, year2)
+# print(f"Number of years between {year1} and {year2} is: {leaps} years" )
+# print("=============================================")
+
+
+# year = int(input("Enter the year to display: "))
+# print(calendar.prcal(year))
+
+
+# cal = calendar.TextCalendar(calendar.SUNDAY)
+# for i in cal.itermonthdays(2002, 1):
+#     print(i)
+
+
+# print("MOTHS OF THE YEAR ARE:")
+# for name in calendar.month_name:
+#     print(name, "\n")
+
+# print("DAYS OF THE WEEK ARE")   
+# for name in calendar.day_name:
+#     print(name, "\n")
+
+
+# with open("C:/Users/user/student-result/hackerrank/calendar.html", "w") as cal:
+#     c = calendar.HTMLCalendar(calendar.SUNDAY)
+#     cal.write(c.formatmonth(2020, 12))
